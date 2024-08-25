6cb57c81b062571301eb843ff1cb195392b4aa29
Harry <tamkt2274@hotmail.com>
2024-08-25 21:42:53 +0700

    Refactored

--- include/util/tty.h
+++ include/util/tty.h
@@ -4,5 +4,4 @@ void print(char *str);
 void println(char *str);
 void print_color(char *str, char *color_code);
 void println_color(char *str, char *color_code);
-void renderImage();
 void clrscr();
