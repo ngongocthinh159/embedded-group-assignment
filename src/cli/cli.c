bc9cb66f384f9dd49578861be15abc8263360deb
Nguyen Do <khoanguyen.do@outlook.com>
2024-08-25 20:29:44 +0700

    feat: implement history

--- src/cli/cli.c
+++ src/cli/cli.c
@@ -1,79 +1,101 @@
 #include "cli/cli.h"
-
 #include "cli/help_text.h"
 #include "cli/welcome_text.h"
-#include "cli/command.h"
+#include "lib/color.h"
+#include "lib/command.h"
+#include "lib/kernel.h"
+#include "lib/stack.h"
 #include "lib/uart.h"
 #include "util/string.h"
 #include "util/tty.h"
-#include "lib/stack.h"
-#include "lib/color.h"
 
 char *OS_NAME = "FIRE_OS";
 char *GROUP_NAME = "6";
 
 const unsigned int COMMAND_MAX_SIZE = 200;
-char command[201]; // global buffer for command shell: after strip from stack buffer (size + 1 for '\0' character)
+char command[201]; // global buffer for command shell: after strip from stack
+                   // buffer (size + 1 for '\0' character)
 
-char stack_buffer[200]; // global buffer for command shell: record all user input
-Stack *cmd_st; // wrapper for command shell buffer (when wrapped the buffer can be treated as a stack and use like a stack)
+char
+    stack_buffer[200]; // global buffer for command shell: record all user input
+Stack *cmd_st; // wrapper for command shell buffer (when wrapped the buffer can
+               // be treated as a stack and use like a stack)
 
-volatile int current_mode = CLI;  // mode switching in kernel.c
+volatile int current_mode = CLI; // mode switching in kernel.c
 
 int welcome() {
-    println(welcome_text);
-    println("");
-    println("");
-    print("Group ");
-    print(GROUP_NAME);
-    println(" - EEET2490 - Embedded System");
-    println("");
-    return 0;
+  println(welcome_text);
+  println("");
+  println("");
+  print("Group ");
+  print(GROUP_NAME);
+  println(" - EEET2490 - Embedded System");
+  println("");
+  return 0;
 }
 
 void print_prefix() {
-    print_color(OS_NAME, CMD_COLOR_RED);
-    print_color(" > ", CMD_COLOR_RED);
+  print_color(OS_NAME, CMD_COLOR_RED);
+  print_color(" > ", CMD_COLOR_RED);
 }
 
 void _handle_internal() {
-    if (str_equal(command, CMD_HELP)) {
-        print(help_text);
-    } else if (str_equal(command, CMD_EXIT)) {
-        // exit();
-    } else if (str_equal(command, CMD_CLEAR)) {
-        clrscr();
+
+  if (str_equal(command, CMD_HELP)) {
+    print(help_text);
+  } else if (str_equal(command, CMD_EXIT)) {
+    // exit();
+  } else if (str_equal(command, CMD_CLEAR)) {
+    clrscr();
+  } else if (str_equal(command, CMD_HISTORY)) {
+    for (int i = 0; i < HISTORY_LENGTH; i++) {
+      uart_dec(i);
+      print(". ");
+      println(history_buffer[i]);
     }
+  }
 }
 
 void handle_cli_mode() {
-    uart_puts("\n\nCLI mode!\n");
-    print_prefix();
+  uart_puts("\n\nCLI mode!\n");
+  print_prefix();
 
-    while (is_cli_mode()) {
-        uart_scanning();  // always scanning for new char
+  while (is_cli_mode()) {
+    uart_scanning(); // always scanning for new char
 
-        if (is_there_new_line()) {
-            get_line(command);
+    if (is_there_ansi_escape() == 2) {
+      get_ansi_control(command);
 
-            print("Command received: ");
-            println(command);
+      print("ANSI escape sequence received: ");
+      println(command);
 
-            // flow control
-            if (str_equal(command, CMD_SWITCH_TO_VIDEO_PLAYER_MODE)) {
-                switch_to_video_player_mode();
-                break;
-            } else if (str_equal(command, CMD_SWITCH_TO_GAME_MODE)) {
-                switch_to_game_mode();
-                break;
-            }
-
-            // handle command in cli mode
-            _handle_internal();
-
-            print_prefix();
-        }
+      _handle_internal();
+      print_prefix();
     }
+
+    if (is_there_new_line()) {
+      get_line(command);
+      // push command to history, similar to bash history
+      cir_buf_push(command);
+
+      print("Command received: ");
+      println(command);
+
+      // flow control
+      if (str_equal(command, CMD_SWITCH_TO_VIDEO_PLAYER_MODE)) {
+        switch_to_video_player_mode();
+        break;
+      } else if (str_equal(command, CMD_SWITCH_TO_GAME_MODE)) {
+        switch_to_game_mode();
+        break;
+      }
+
+      // handle command in cli mode
+      _handle_internal();
+
+      print_prefix();
+    }
+  }
 }
 
 void switch_to_cli_mode() { current_mode = CLI; }
