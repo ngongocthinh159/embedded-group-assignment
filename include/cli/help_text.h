536ddcad83085d4cf984b487a5891ba08a07ec65
Nguyen Do <khoanguyen.do@outlook.com>
2024-08-25 16:43:28 +0700

    refactor: clangd-format

--- include/cli/help_text.h
+++ include/cli/help_text.h
@@ -1,12 +1,9 @@
-#pragma once 
+#pragma once
 
-char help_text[] = 
-"For more information on a specific command, type help <command-name> \n"
-"playvideo: Playing a video\n"
-"playgame: initialise the pre-installed video game \n"
-"showinfo: show board revision and MAC address \n"
-"clear: clear the command line interface \n"
-"exit: exit the OS \n";
-
-
-
+char help_text[] =
+    "For more information on a specific command, type help <command-name> \n"
+    "playvideo: Playing a video\n"
+    "playgame: initialise the pre-installed video game \n"
+    "showinfo: show board revision and MAC address \n"
+    "clear: clear the command line interface \n"
+    "exit: exit the OS \n";
