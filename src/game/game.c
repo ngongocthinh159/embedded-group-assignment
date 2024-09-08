43cc5ffbe7391fc995dd925c90afc3b8264808c7
Tkphuc <s3927187@rmit.edu.vn>
2024-09-09 04:43:50 +0700

    reformat with prettier

--- src/game/game.c
+++ src/game/game.c
@@ -1,8 +1,6 @@
 #include "../../include/game/game_mode.h"
 #include "../../include/game/game.h"
 
-
-
 #include "cli/cli.h"
 #include "cli/command.h"
 #include "lib/color.h"
@@ -14,21 +12,35 @@
 #include "lib/keyboard.h"
 #include "game/game.h"
 
-int _handle_game_mode_internal() {
-    draw_general_menu();
-  if (_is_up_command()) {
+int _handle_game_mode_internal()
+{
+  draw_general_menu();
+  if (_is_up_command())
+  {
     println("UP");
-  } else if (_is_down_command()) {
+  }
+  else if (_is_down_command())
+  {
     println("DOWN");
-  } else if (_is_left_command()) {
+  }
+  else if (_is_left_command())
+  {
     println("LEFT");
-  } else if (_is_right_command()) {
+  }
+  else if (_is_right_command())
+  {
     println("RIGHT");
-  } else if (_is_enter_or_space_command()) {
+  }
+  else if (_is_enter_or_space_command())
+  {
     println("SPACE or ENTER");
-  } else if (_is_back_tick_command()) {
+  }
+  else if (_is_back_tick_command())
+  {
     println("BACK TICK");
-  } else {  
+  }
+  else
+  {
     _print_error_game_mode();
   }
 
@@ -37,23 +49,27 @@ int _handle_game_mode_internal() {
   return 1;
 }
 
-void handle_game_mode() {
-    
-    //displayGamePlayScreen();
-    
+void handle_game_mode()
+{
+
+  // displayGamePlayScreen();
+
   print_color("\n\nGame mode!\n", CMD_COLOR_YEL);
   print_prefix();
 
-  while (is_game_mode()) {
-    uart_scanning();  // always scanning for new char
-   
-    if (is_there_ansi_escape()) {
+  while (is_game_mode())
+  {
+    uart_scanning(); // always scanning for new char
+
+    if (is_there_ansi_escape())
+    {
       get_ansi_control(command);
 
       _handle_game_mode_internal();
     }
 
-    if (is_there_new_line()) {
+    if (is_there_new_line())
+    {
       get_line(command);
 
       _handle_game_mode_internal();
@@ -61,37 +77,45 @@ void handle_game_mode() {
   }
 }
 
-void _print_error_game_mode() {
+void _print_error_game_mode()
+{
   print_color("Unknown command received in ", CMD_COLOR_RED);
   print_color("GAME", CMD_COLOR_GRN);
   print_color(" mode!\n", CMD_COLOR_RED);
 }
 
-int _is_up_command() {
+int _is_up_command()
+{
   return strstr(command, CSI_CUU) || str_equal(command, "w");
 }
 
-int _is_down_command() {
+int _is_down_command()
+{
   return strstr(command, CSI_CUD) || str_equal(command, "s");
 }
 
-int _is_left_command() {
+int _is_left_command()
+{
   return strstr(command, CSI_CUB) || str_equal(command, "a");
 }
 
-int _is_right_command() {
+int _is_right_command()
+{
   return strstr(command, CSI_CUF) || str_equal(command, "d");
 }
 
-int _is_back_tick_command() {
+int _is_back_tick_command()
+{
   return str_equal(command, BACK_TICK_STR);
 }
 
-int _is_enter_or_space_command() {
+int _is_enter_or_space_command()
+{
   return str_equal(command, SPACE_STR);
 }
 
-void _exit_game() {
+void _exit_game()
+{
   drawRectARGB32(0, 0, width - 1, height - 1, COLOR_BLACK, 1);
   switch_to_cli_mode();
 }
--- src/game/game_mode.c
+++ src/game/game_mode.c
@@ -1,54 +1,70 @@
-#include"../../include/image-renderer/image.h"
+#include "../../include/image-renderer/image.h"
 #include "../../include/lib/framebf.h"
 #include "../../include/game/screen/game-mode/welcome_screen_easy.h"
 #include "../../include/game/screen/game-mode/welcome_screen_medium.h"
 #include "../../include/game/screen/game-mode/welcome_screen_hard.h"
 #include "../../include/game/screen/game-mode/background.h"
-//unsigned long *mode_menu;
-char normal_mode []={"CURRENT DIFFICULTY: NORMAL"};
-//char medium_mode []={"CURRENT DIFFICULTY: MEDIUM"};
-char hard_mode []={"CURRENT DIFFICULTY: HARD"};
-//char *title_ptr = &title;
+// unsigned long *mode_menu;
+char normal_mode[] = {"CURRENT DIFFICULTY: NORMAL"};
+// char medium_mode []={"CURRENT DIFFICULTY: MEDIUM"};
+char hard_mode[] = {"CURRENT DIFFICULTY: HARD"};
+// char *title_ptr = &title;
 
+struct menu_
+{
+    unsigned int x
+};
 
-void draw_new_game_highlight(){
-    drawLineWithThicknessARGB32(430,355,430,405,0xFFFFFF,1);//left line of new game, added 30 pixel to x coors compare to canva soruce
-    drawLineWithThicknessARGB32(610,355,610,405,0xFFFFFF,1);//right line of new game
+void draw_new_game_highlight()
+{
+    drawLineWithThicknessARGB32(430, 355, 430, 405, 0xFFFFFF, 1); // left line of new game, added 30 pixel to x coors compare to canva soruce
+    drawLineWithThicknessARGB32(610, 355, 610, 405, 0xFFFFFF, 1); // right line of new game
 }
-void draw_difficulty_highlight(){
-    drawString(265,440,normal_mode,0xFFFFFF,1); // normal mode as default
-    drawLineWithThicknessARGB32(779,430,779,480,0xFFFFFF,1);// right line of difficulty
-    drawLineWithThicknessARGB32(254,430,254,480,0xFFFFFF,1);//left line of difficulty
+void draw_difficulty_highlight()
+{
+    drawString(265, 440, normal_mode, 0xFFFFFF, 1);               // normal mode as default
+    drawLineWithThicknessARGB32(779, 430, 779, 480, 0xFFFFFF, 1); // right line of difficulty
+    drawLineWithThicknessARGB32(254, 430, 254, 480, 0xFFFFFF, 1); // left line of difficulty
 }
-void draw_exit_highlight(){
-    drawLineWithThicknessARGB32(430,510,430,560,0xFFFFFF,1);//left line of new game, added 30 pixel to x coors compare to canva soruce
-    drawLineWithThicknessARGB32(610,510,610,560,0xFFFFFF,1);//right line of new game
+void draw_difficulty()
+{
+    drawString(265, 440, normal_mode, 0xFFFFFF, 1); // normal mode as default
 }
-void draw_general_menu(){
-    drawString(380,240,"TETRIS",0xff4D3D,3);
-    drawString(450,520,"EXIT GAME",0xFFFFFF,1);
-    drawString(450,365,"NEW GAME",0xFFFFFF,1);
-    
-    draw_difficulty_highlight();
-    
-    //drawRectARGB32(212,33,801,734,0x0000FF,0);
-    //drawImage(background,212,34,600,700);
-    drawRectARGB32(240,184,785,584,0x0000FF,0); // draw outer rectangle
+void draw_exit_highlight()
+{
+    drawLineWithThicknessARGB32(430, 510, 430, 560, 0xFFFFFF, 1); // left line of new game, added 30 pixel to x coors compare to canva soruce
+    drawLineWithThicknessARGB32(610, 510, 610, 560, 0xFFFFFF, 1); // right line of new game
+}
+void draw_general_menu()
+{
+    drawString(380, 240, "TETRIS", 0xff4D3D, 3);
+    drawString(450, 520, "EXIT GAME", 0xFFFFFF, 1);
+    drawString(450, 365, "NEW GAME", 0xFFFFFF, 1);
 
+    draw_difficulty();
+    // draw_difficulty_highlight();
+
+    // drawRectARGB32(212,33,801,734,0x0000FF,0);
+    // drawImage(background,212,34,600,700);
+    drawRectARGB32(240, 184, 785, 584, 0x0000FF, 0); // draw outer rectangle
 }
-void draw_nav_exit(){
+void draw_nav_exit()
+{
     draw_general_menu();
     draw_exit_highlight();
 }
-void draw_nav_new_game(){ // new game element highlighted
+void draw_nav_new_game()
+{ // new game element highlighted
     draw_general_menu();
     draw_new_game_highlight();
 }
-void draw_nav_diffuclty(){
+void draw_nav_diffuclty()
+{
     draw_general_menu();
     draw_exit_highlight();
 }
-void draw_hard_difficultu(){
+void draw_hard_difficultu()
+{
     draw_general_menu();
     draw_exit_highlight();
 }
@@ -57,8 +73,8 @@ void draw_hard_difficultu(){
 void read_from_screen(){
     int array_length = sizeof(mode_menu) / sizeof(unsigned long);
     mode_menu = (unsigned long *)
-    
+
     for(int i =0;i<array_length;i++){
-        mode_menu[i] = 
+        mode_menu[i] =
     }
 }*/
