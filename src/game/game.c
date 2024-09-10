24594fcc4ea9b82a50d6ff6e18fdd5d0904261da
Thinh Ngo Ngoc <ngongocthinh124@gmail.com>
2024-09-10 22:49:59 +0700

    refractor

--- src/game/game.c
+++ src/game/game.c
@@ -1,15 +1,16 @@
+#include "game/game.h"
+
 #include "cli/cli.h"
 #include "cli/command.h"
 #include "lib/color.h"
 #include "lib/framebf.h"
+#include "lib/keyboard.h"
+#include "lib/math.h"
+#include "lib/timer.h"
 #include "lib/uart.h"
+#include "util/cirbuf.h"
 #include "util/string.h"
 #include "util/tty.h"
-#include "util/cirbuf.h"
-#include "lib/keyboard.h"
-#include "game/game.h"
-#include "lib/timer.h"
-#include "lib/math.h"
 
 /* Function prototypes */
 void _uart_scanning_callback();
@@ -21,7 +22,7 @@ void _handle_events_call_every_200ms();
 void _handle_events_call_every_500ms();
 void _handle_events_call_every_1s();
 
-/* Global variables */  
+/* Global variables */
 int should_exit_game_mode = 0;
 const int smallest_interval_ms = 10;
 
@@ -33,11 +34,21 @@ const unsigned int OFFSET_PHYSICAL_NEXT_FRAME_Y = 544;
 
 /* call_every_ms should be multiple of 10 */
 volatile Event events[] = {
-  {.call_every_ms = 50, .handler = _handle_events_call_every_50ms, .counter = 0 },
-  {.call_every_ms = 100, .handler = _handle_events_call_every_100ms, .counter = 0 },
-  {.call_every_ms = 200, .handler = _handle_events_call_every_200ms, .counter = 0 },
-  {.call_every_ms = 500, .handler = _handle_events_call_every_500ms, .counter = 0 },
-  {.call_every_ms = 1000, .handler = _handle_events_call_every_1s, .counter = 0 },
+    {.call_every_ms = 50,
+     .handler = _handle_events_call_every_50ms,
+     .counter = 0},
+    {.call_every_ms = 100,
+     .handler = _handle_events_call_every_100ms,
+     .counter = 0},
+    {.call_every_ms = 200,
+     .handler = _handle_events_call_every_200ms,
+     .counter = 0},
+    {.call_every_ms = 500,
+     .handler = _handle_events_call_every_500ms,
+     .counter = 0},
+    {.call_every_ms = 1000,
+     .handler = _handle_events_call_every_1s,
+     .counter = 0},
 };
 
 /* Game init */
@@ -47,66 +58,75 @@ static const int VIRTUAL_GAME_FIELD_OFFSET = 3;
 static const int BLOCK_SIZE = 30;
 
 static Point init_pos_shape_I[] = {
-  {.x = 0, .y = 0},
-  {.x = 1, .y = 0},
-  {.x = 2, .y = 0},
-  {.x = 3, .y = 0},
+    {.x = 0, .y = 0},
+    {.x = 1, .y = 0},
+    {.x = 2, .y = 0},
+    {.x = 3, .y = 0},
 };
 static Point init_center_shape_I = {.x = 1, .y = 0};
 
 static Point init_pos_shape_O[] = {
-  {.x = 0, .y = 0},
-  {.x = 1, .y = 0},
-  {.x = 0, .y = 1},
-  {.x = 1, .y = 1},
+    {.x = 0, .y = 0},
+    {.x = 1, .y = 0},
+    {.x = 0, .y = 1},
+    {.x = 1, .y = 1},
 };
 static Point init_center_shape_O = {.x = 0, .y = 0};
 
 static Point init_pos_shape_T[] = {
-  {.x = 1, .y = 0},
-  {.x = 0, .y = 1},
-  {.x = 1, .y = 1},
-  {.x = 2, .y = 1},
+    {.x = 1, .y = 0},
+    {.x = 0, .y = 1},
+    {.x = 1, .y = 1},
+    {.x = 2, .y = 1},
 };
 static Point init_center_shape_T = {.x = 1, .y = 1};
 
 static Point init_pos_shape_S[] = {
-  {.x = 0, .y = 1},
-  {.x = 1, .y = 1},
-  {.x = 1, .y = 0},
-  {.x = 2, .y = 0},
+    {.x = 0, .y = 1},
+    {.x = 1, .y = 1},
+    {.x = 1, .y = 0},
+    {.x = 2, .y = 0},
 };
 static Point init_center_shape_S = {.x = 1, .y = 1};
 
 static Point init_pos_shape_Z[] = {
-  {.x = 0, .y = 0},
-  {.x = 1, .y = 0},
-  {.x = 1, .y = 1},
-  {.x = 2, .y = 1},
+    {.x = 0, .y = 0},
+    {.x = 1, .y = 0},
+    {.x = 1, .y = 1},
+    {.x = 2, .y = 1},
 };
 static Point init_center_shape_Z = {.x = 1, .y = 1};
 
 static Point init_pos_shape_J[] = {
-  {.x = 0, .y = 0},
-  {.x = 0, .y = 1},
-  {.x = 1, .y = 1},
-  {.x = 2, .y = 1},
+    {.x = 0, .y = 0},
+    {.x = 0, .y = 1},
+    {.x = 1, .y = 1},
+    {.x = 2, .y = 1},
 };
 static Point init_center_shape_J = {.x = 1, .y = 1};
 
 static Point init_pos_shape_L[] = {
-  {.x = 2, .y = 0},
-  {.x = 0, .y = 1},
-  {.x = 1, .y = 1},
-  {.x = 2, .y = 1},
+    {.x = 2, .y = 0},
+    {.x = 0, .y = 1},
+    {.x = 1, .y = 1},
+    {.x = 2, .y = 1},
 };
 static Point init_center_shape_L = {.x = 1, .y = 1};
 
 Point points_buffer_angle_rotated[__size];
-Point points_buffer_angle_0[__size]; // DO NOT USE THIS BUFFER: Only for get init position
+Point points_buffer_angle_0[__size];  // DO NOT USE THIS BUFFER: Only for get
+                                      // init position
 Point center_point_buffer;
 
-Piece dynamic_piece = {.shape = SHAPE_I, .color = RED, .center_point = {.x = 1, .y = 0}, .angle = ANGLE_0};
+Piece dynamic_piece = {.shape = SHAPE_I,
+                       .color = RED,
+                       .center_point = {.x = 1, .y = 0},
+                       .angle = ANGLE_0};
+
+Piece next_piece = {.shape = SHAPE_I,
+                       .color = RED,
+                       .center_point = {.x = 1, .y = 0},
+                       .angle = ANGLE_0};
 
 void handle_game_mode() {
   print_color("\n\nGame mode!\n", CMD_COLOR_YEL);
@@ -139,22 +159,19 @@ void _uart_scanning_callback() {
 }
 
 void _handle_timing_events() {
-  for (int i = 0; i < sizeof(events)/sizeof(Event); i++) {
+  for (int i = 0; i < sizeof(events) / sizeof(Event); i++) {
     events[i].counter++;
-    if (events[i].counter >= events[i].call_every_ms/smallest_interval_ms + 1) {
+    if (events[i].counter >=
+        events[i].call_every_ms / smallest_interval_ms + 1) {
       events[i].counter = 0;
       events[i].handler();
     }
   }
 }
 
-void _handle_events_call_every_50ms() {
+void _handle_events_call_every_50ms() {}
 
-}
-
-void _handle_events_call_every_100ms() {
-  
-}
+void _handle_events_call_every_100ms() {}
 
 void _handle_events_call_every_200ms() {
   _clear_game_piece(&dynamic_piece);
@@ -162,13 +179,9 @@ void _handle_events_call_every_200ms() {
   _draw_game_piece(&dynamic_piece);
 }
 
-void _handle_events_call_every_500ms() {
+void _handle_events_call_every_500ms() {}
 
-}
-
-void _handle_events_call_every_1s() {
-  
-}
+void _handle_events_call_every_1s() {}
 
 void _handle_game_mode_internal() {
   if (_is_up_command()) {
@@ -186,7 +199,7 @@ void _handle_game_mode_internal() {
     println("SPACE or ENTER");
   } else if (_is_back_tick_command()) {
     println("BACK TICK");
-  } else {  
+  } else {
     _print_error_game_mode();
   }
 
@@ -215,13 +228,9 @@ int _is_right_command() {
   return strstr(command, CSI_CUF) || str_equal(command, KEYBOARD_D);
 }
 
-int _is_back_tick_command() {
-  return str_equal(command, BACK_TICK_STR);
-}
+int _is_back_tick_command() { return str_equal(command, BACK_TICK_STR); }
 
-int _is_enter_or_space_command() {
-  return str_equal(command, SPACE_STR);
-}
+int _is_enter_or_space_command() { return str_equal(command, SPACE_STR); }
 
 void _exit_game() {
   drawRectARGB32(0, 0, width - 1, height - 1, COLOR_BLACK, 1);
@@ -230,22 +239,21 @@ void _exit_game() {
 
 void _init_game() {
   displayGamePlayScreen();
-  _spawn_random_dynamic_piece();
+  _spawn_random_piece_to(&dynamic_piece);
+  _spawn_random_piece_to(&next_piece);
+  _draw_next_frame_piece(&next_piece);
 }
 
-void _increase_current_piece() {
-  dynamic_piece.center_point.y += 1;
-}
+void _increase_current_piece() { dynamic_piece.center_point.y += 1; }
 
-void _draw_game_state() {
-  _draw_game_piece(&dynamic_piece);
-}
+void _draw_game_state() { _draw_game_piece(&dynamic_piece); }
 
 void _clear_game_piece(Piece *piece) {
   _copy_piece_rotated_points_to_buffer(piece, points_buffer_angle_rotated);
   _adjust_center_point_if_overflow(piece, points_buffer_angle_rotated);
   for (int i = 0; i < __size; i++) {
-    draw_game_point(points_buffer_angle_rotated[i].x, points_buffer_angle_rotated[i].y, CLEAR);
+    _draw_game_point(points_buffer_angle_rotated[i].x,
+                    points_buffer_angle_rotated[i].y, CLEAR);
   }
 }
 
@@ -253,12 +261,13 @@ void _draw_game_piece(Piece *piece) {
   _copy_piece_rotated_points_to_buffer(piece, points_buffer_angle_rotated);
   _adjust_center_point_if_overflow(piece, points_buffer_angle_rotated);
   for (int i = 0; i < __size; i++) {
-    draw_game_point(points_buffer_angle_rotated[i].x, points_buffer_angle_rotated[i].y, piece->color);
+    _draw_game_point(points_buffer_angle_rotated[i].x,
+                    points_buffer_angle_rotated[i].y, piece->color);
   }
 }
 
-void draw_game_point(int x, int y, Color color) {
-  if (y <= VIRTUAL_GAME_FIELD_OFFSET) { // no draw virtual point
+void _draw_game_point(int x, int y, Color color) {
+  if (y <= VIRTUAL_GAME_FIELD_OFFSET) {  // no draw virtual point
     return;
   }
 
@@ -267,8 +276,10 @@ void draw_game_point(int x, int y, Color color) {
 
   if (real_point_y >= GAME_FIELD_HEIGHT) return;
 
-  int physical_point_x = OFFSET_PHYSICAL_GAME_FIELD_X + real_point_x*BLOCK_SIZE;
-  int physical_point_y = OFFSET_PHYSICAL_GAME_FIELD_Y + real_point_y*BLOCK_SIZE;
+  int physical_point_x =
+      OFFSET_PHYSICAL_GAME_FIELD_X + real_point_x * BLOCK_SIZE;
+  int physical_point_y =
+      OFFSET_PHYSICAL_GAME_FIELD_Y + real_point_y * BLOCK_SIZE;
   if (color == CYAN) {
     drawCyanBlock(physical_point_x, physical_point_y);
   } else if (color == YELLOW) {
@@ -289,8 +300,8 @@ void draw_game_point(int x, int y, Color color) {
 }
 
 // init points with center point move out of init center point
-Point* _copy_piece_angle_0_points_to_buffer(Piece *piece, Point buffer[]) {
-  Point* init_points = _get_init_points(piece);
+Point *_copy_piece_angle_0_points_to_buffer(Piece *piece, Point buffer[]) {
+  Point *init_points = _get_init_points(piece);
   Point init_center_point = _get_init_center_point(piece);
 
   int adjust_x = piece->center_point.x - init_center_point.x;
@@ -303,7 +314,7 @@ Point* _copy_piece_angle_0_points_to_buffer(Piece *piece, Point buffer[]) {
 }
 
 // init points with init center point (zero point spawn position)
-Point* _get_init_points(Piece *piece) {
+Point *_get_init_points(Piece *piece) {
   if (piece->shape == SHAPE_I) {
     return init_pos_shape_I;
   } else if (piece->shape == SHAPE_O) {
@@ -381,13 +392,16 @@ void _adjust_center_point_if_overflow(Piece *piece, Point points[]) {
 }
 
 void _copy_piece_rotated_points_to_buffer(Piece *piece, Point buffer[]) {
-  Point* _angle_0_init_points = _copy_piece_angle_0_points_to_buffer(piece, points_buffer_angle_0);
+  Point *_angle_0_init_points =
+      _copy_piece_angle_0_points_to_buffer(piece, points_buffer_angle_0);
   for (int i = 0; i < __size; i++) {
     int x_0 = _angle_0_init_points[i].x - piece->center_point.x;
     int y_0 = _angle_0_init_points[i].y - piece->center_point.y;
 
-    int x_1 = x_0 * _get_angle_multiplier_cos(piece->angle) - y_0 * _get_angle_multiplier_sin(piece->angle);
-    int y_1 = x_0 * _get_angle_multiplier_sin(piece->angle) - y_0 * _get_angle_multiplier_cos(piece->angle);
+    int x_1 = x_0 * _get_angle_multiplier_cos(piece->angle) -
+              y_0 * _get_angle_multiplier_sin(piece->angle);
+    int y_1 = x_0 * _get_angle_multiplier_sin(piece->angle) -
+              y_0 * _get_angle_multiplier_cos(piece->angle);
 
     x_1 += piece->center_point.x;
     y_1 += piece->center_point.y;
@@ -421,100 +435,87 @@ int _get_angle_multiplier_cos(Angle angle) {
   }
 }
 
-void _spawn_random_dynamic_piece() {
+void _spawn_random_piece_to(Piece *piece) {
   int rand_angle = rand(1, 4);
   int rand_color = rand(1, 7);
   int rand_shape = rand(1, 7);
   int rand_center_point_x = rand(0, GAME_FIELD_WIDHT - 1);
-  
+
   if (rand_angle == 1) {
-    dynamic_piece.angle = ANGLE_0;
+    piece->angle = ANGLE_0;
   } else if (rand_angle == 2) {
-    dynamic_piece.angle = ANGLE_90;
+    piece->angle = ANGLE_90;
   } else if (rand_angle == 3) {
-    dynamic_piece.angle = ANGLE_180;
+    piece->angle = ANGLE_180;
   } else if (rand_angle == 4) {
-    dynamic_piece.angle = ANGLE_270;
+    piece->angle = ANGLE_270;
   }
 
   if (rand_color == 1) {
-    dynamic_piece.color = CYAN;
+    piece->color = CYAN;
   } else if (rand_color == 2) {
-    dynamic_piece.color = YELLOW;
+    piece->color = YELLOW;
   } else if (rand_color == 3) {
-    dynamic_piece.color = PURPLE;
+    piece->color = PURPLE;
   } else if (rand_color == 4) {
-    dynamic_piece.color = GREEN;
+    piece->color = GREEN;
   } else if (rand_color == 5) {
-    dynamic_piece.color = RED;
+    piece->color = RED;
   } else if (rand_color == 6) {
-    dynamic_piece.color = BLUE;
+    piece->color = BLUE;
   } else if (rand_color == 7) {
-    dynamic_piece.color = ORANGE;
+    piece->color = ORANGE;
   }
 
   if (rand_shape == 1) {
-    dynamic_piece.shape = SHAPE_I;
+    piece->shape = SHAPE_I;
   } else if (rand_shape == 2) {
-    dynamic_piece.shape = SHAPE_O;
+    piece->shape = SHAPE_O;
   } else if (rand_shape == 3) {
-    dynamic_piece.shape = SHAPE_T;
+    piece->shape = SHAPE_T;
   } else if (rand_shape == 4) {
-    dynamic_piece.shape = SHAPE_S;
+    piece->shape = SHAPE_S;
   } else if (rand_shape == 5) {
-    dynamic_piece.shape = SHAPE_Z;
+    piece->shape = SHAPE_Z;
   } else if (rand_shape == 6) {
-    dynamic_piece.shape = SHAPE_J;
+    piece->shape = SHAPE_J;
   } else if (rand_shape == 7) {
-    dynamic_piece.shape = SHAPE_L;
+    piece->shape = SHAPE_L;
   }
 
-  dynamic_piece.center_point.x = rand_center_point_x;
-  dynamic_piece.center_point.y = 0;
+  piece->center_point.x = rand_center_point_x;
+  piece->center_point.y = 0;
 
-  _copy_piece_rotated_points_to_buffer(&dynamic_piece, points_buffer_angle_rotated);
-  _adjust_center_point_if_overflow(&dynamic_piece, points_buffer_angle_rotated);
+  piece->shape = SHAPE_L;
+
+  _copy_piece_rotated_points_to_buffer(piece,
+                                       points_buffer_angle_rotated);
+  _adjust_center_point_if_overflow(piece, points_buffer_angle_rotated);
 }
 
 void _draw_next_frame_piece(Piece *piece) {
-  Point* points = _get_init_points(piece);
+  Point *points = _get_init_points(piece);
   for (int i = 0; i < __size; i++) {
-    _draw_next_frame_point(points[i].x, points[i].y, piece->color, piece->shape);
+    int final_x = points[i].x;
+    int final_y = points[i].y;
+    _adjust_x_y_for_center_drawing_next_frame(&final_x, &final_y, piece->shape);
+    _draw_next_frame_point(final_x, final_y, piece->color);
   }
 }
 
 void _clear_next_frame_piece(Piece *piece) {
-  Point* points = _get_init_points(piece);
+  Point *points = _get_init_points(piece);
   for (int i = 0; i < __size; i++) {
-    _draw_next_frame_point(points[i].x, points[i].y, CLEAR, piece->shape);
+    int final_x = points[i].x;
+    int final_y = points[i].y;
+    _adjust_x_y_for_center_drawing_next_frame(&final_x, &final_y, piece->shape);
+    _draw_next_frame_point(final_x, final_y, CLEAR);
   }
 }
 
-void _draw_next_frame_point(int x, int y, Color color, Shape shape) {
-  int physical_point_x = OFFSET_PHYSICAL_NEXT_FRAME_X + x*BLOCK_SIZE;
-  int physical_point_y = OFFSET_PHYSICAL_NEXT_FRAME_Y + y*BLOCK_SIZE;
-  
-  if (shape == SHAPE_I) {
-    physical_point_y += 2*BLOCK_SIZE;
-  } else if (shape == SHAPE_O) {
-    physical_point_x += BLOCK_SIZE;
-    physical_point_y += BLOCK_SIZE;
-  } else if (shape == SHAPE_T) {
-    physical_point_x += BLOCK_SIZE;
-    physical_point_y += BLOCK_SIZE;
-  } else if (shape == SHAPE_S) {
-    physical_point_x += BLOCK_SIZE;
-    physical_point_y += BLOCK_SIZE;
-  } else if (shape == SHAPE_Z) {
-    physical_point_x += BLOCK_SIZE;
-    physical_point_y += BLOCK_SIZE;
-  } else if (shape == SHAPE_J) {
-    physical_point_x += BLOCK_SIZE;
-    physical_point_y += BLOCK_SIZE;
-  } else {
-    physical_point_x += BLOCK_SIZE;
-    physical_point_y += BLOCK_SIZE;
-  }
+void _draw_next_frame_point(int x, int y, Color color) {
+  int physical_point_x = OFFSET_PHYSICAL_NEXT_FRAME_X + x * BLOCK_SIZE;
+  int physical_point_y = OFFSET_PHYSICAL_NEXT_FRAME_Y + y * BLOCK_SIZE;
 
   if (color == CYAN) {
     drawCyanBlock(physical_point_x, physical_point_y);
@@ -532,5 +533,29 @@ void _draw_next_frame_point(int x, int y, Color color, Shape shape) {
     drawOrangeBlock(physical_point_x, physical_point_y);
   } else if (color == CLEAR) {
     drawClearBlock(physical_point_x, physical_point_y);
+  }
+}
+
+void _adjust_x_y_for_center_drawing_next_frame(int *x, int *y, Shape shape) {
+  if (shape == SHAPE_I) {
+    *y += 2;
+  } else if (shape == SHAPE_O) {
+    *x += 1;
+    *y += 1;
+  } else if (shape == SHAPE_T) {
+    *x += 1;
+    *y += 1;
+  } else if (shape == SHAPE_S) {
+    *x += 1;
+    *y += 1;
+  } else if (shape == SHAPE_Z) {
+    *x += 1;
+    *y += 1;
+  } else if (shape == SHAPE_J) {
+    *x += 1;
+    *y += 1;
+  } else {
+    *x += 1;
+    *y += 1;
   }
 }
